16fef31cb946a55214916fe66c126e99c98fb50a
Noureddine-Elmeghary <elmegharynoureddine000@gmail.com>
2025-05-24 15:42:22 +0100

    Commit

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,13 +1,3 @@
-import yfinance as yf
-
-# Download Tesla stock data
-tesla_data = yf.download('TSLA')
-
-# Reset the index to move Date from index to a column
-tesla_data.reset_index(inplace=True)
-
-# Display the first 5 rows
-print(tesla_data.head())
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
